1473c43aa69539907506833bcfe152dd2f5f6271
Kenny Pflug <kenny.pflug@live.de>
2022-01-15 07:52:15 +0100

    added test that imports all five-letter words to RavenDB

---
+++ WordleWords.Tests/RavenDb.cs
@@ -0,0 +1,39 @@
+﻿using Raven.Client.Documents;
+using Raven.Client.Documents.Conventions;
+using Raven.Client.Documents.Session;
+using Synnotech.Xunit;
+
+namespace WordleWords.Tests;
+
+public static class RavenDb
+{
+    private static object LockObject { get; } = new ();
+    
+    private static IDocumentStore? DocumentStore { get; set; }
+
+    public static IDocumentSession CreateSession() =>
+        GetOrCreateDocumentStore().OpenSession();
+
+    private static IDocumentStore GetOrCreateDocumentStore()
+    {
+        var documentStore = DocumentStore;
+        if (documentStore != null)
+            return documentStore;
+
+        lock (LockObject)
+        {
+            documentStore = DocumentStore;
+            if (documentStore != null)
+                return documentStore;
+
+            documentStore = DocumentStore = new DocumentStore
+            {
+                Urls = new[] { TestSettings.Configuration["ravenDbUrl"] },
+                Database = TestSettings.Configuration["databaseName"],
+                Conventions = new DocumentConventions { IdentityPartsSeparator = '-' }
+            }.Initialize();
+        }
+
+        return documentStore;
+    }
+}
---
+++ WordleWords.Tests/RavenDbImport.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Raven.Client.Documents;
+using Synnotech.Core.Entities;
+using Synnotech.Xunit;
+using Xunit;
+
+namespace WordleWords.Tests;
+
+public static class RavenDbImport
+{
+    [SkippableFact]
+    public static void ImportWordsToRavenDb()
+    {
+        Skip.IfNot(TestSettings.Configuration.GetValue<bool>("runImport"));
+
+        var currentDirectory = Path.GetDirectoryName(typeof(RavenDbImport).Assembly.Location)!;
+        using var streamReader = new StreamReader(Path.Combine(currentDirectory, "five-letter-words.txt"));
+        using var session = RavenDb.CreateSession();
+        string? currentLine;
+        while ((currentLine = streamReader.ReadLine()) != null)
+        {
+            if (currentLine.Length != 5)
+                continue;
+
+            var fiveLetterWord = new FiveLetterWord
+            {
+                Id = currentLine,
+                Character1 = currentLine[0],
+                Character2 = currentLine[1],
+                Character3 = currentLine[2],
+                Character4 = currentLine[3],
+                Character5 = currentLine[4]
+            };
+            session.Store(fiveLetterWord);
+        }
+        
+        session.SaveChanges();
+    }
+}
+
+public sealed class FiveLetterWord : StringEntity
+{
+    public char Character1 { get; init; }
+    public char Character2 { get; init; }
+    public char Character3 { get; init; }
+    public char Character4 { get; init; }
+    public char Character5 { get; init; }
+}
